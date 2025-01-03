3d7614ce2a6b74b81f6a9ac7037a568ad3315ba8
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-01-03 23:54:40 +0530

    `feat: Added API call to fetch now playing movies and updated app store configuration
    * Added API call to fetch now playing movies in Browse component
    * Updated app store configuration to include movies reducer
    * Added API options and authorization token to constants file

--- src/utils/appStore.jsx
+++ src/utils/appStore.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice.jsx";
+import moviesReducer from "./moviesSlice.jsx";
 
 const appStore = configureStore({
     reducer: {
       user: userReducer,
+      movies: moviesReducer,
     },
   });
   
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -3,3 +3,12 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const icon = "https://img.freepik.com/free-vector/cute-man-working-laptop-cartoon-vector-icon-illustration-people-technology-icon-concept-isolated_138676-9123.jpg?ga=GA1.1.1587577946.1735911775&semt=ais_hybrid"
 
 export const head = "https://assets.nflxext.com/ffe/siteui/vlv3/2f5a878d-bbce-451b-836a-398227a34fbf/web/IN-en-20241230-TRIFECTA-perspective_5ab944a5-1a71-4f6d-b341-8699d0491edd_small.jpg"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzNlYTliMWMxMzFiYWM2N2FlNDllOTdiOTZmNmRiYyIsIm5iZiI6MTczNTkyNzE1Ni40NDksInN1YiI6IjY3NzgyNTc0YWI1ZWM0YzNkYzcyNjZjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.179kNRFt4FZ9Xq0e05oUH9iz03uzWtPA6iLluvC9TWw'
+    }
+  };
+  

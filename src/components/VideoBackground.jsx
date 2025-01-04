24feef6cfd94d3ec7b98d7a3f80516098d49a962
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-01-04 10:58:11 +0530

    `Refactor VideoBackground component to use useMovieTrailer hook and update iframe src`

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,40 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer.jsx";
 
 const VideoBackground = ({ movieId }) => {
   // const [trailerId, setTrailerId] = useState(null);
   // fetch trailer VideoBackground
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
-  const dispatch = useDispatch();
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/558449/videos?language=en-US",
-      API_OPTIONS
-    );
-
-    const json = await data.json();
-    console.log(json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    // setTrailerId(trailer.key);
-    console.log(trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+  useMovieTrailer(movieId);
   return (
-    <div>
+    <div className="">
       <iframe
-        width="2000"
-        height="800"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+      className="w-screen aspect-video"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
         title="YouTube video player"
-        frameBorder="0"
+        frameborder = "0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-20">
+    <div className="w-screen aspect-video pt-[14%] px-24 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div className="">
-        <button className="bg-gray-700 text-black cursor-pointer p-4 px-16 text-xl bg-opacity-60 rounded-lg">
+        <button className="bg-white text-black cursor-pointer p-4 px-16 text-xl rounded-lg hover:bg-opacity-70">
           ▶️Play
         </button>
-        <button className="mx-2 bg-gray-700 text-black cursor-pointer p-4 px-16 text-xl rounded-lg bg-opacity-60">
+        <button className="bg-white mx-2 text-black cursor-pointer p-4 px-16 text-xl rounded-lg hover:bg-opacity-70">
           More Info
         </button>
       </div>
